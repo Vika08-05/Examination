0ff8ae8f33f49d64b84f18b241a173d180f16bc9
Vika <vikusagerman@gmail.com>
2021-03-26 00:09:50 +0200

    Add icons

--- src/Components/Main/main.js
+++ src/Components/Main/main.js
@@ -8,17 +8,22 @@ const Main = () => {
             <div className="page">
                 <div className="card">
                     <div className="card-body">
-                        <p className="heading1"> <span className="today">Today</span> <span className="float-right headingright">7h 15min</span> </p>
+                        <p className="heading1"> <span className="today">Today</span> <span className="float-right headingright"></span> </p>
                         <p> <input style={{ marginRight: "1%" }} type="radio" id="huey" name="drone" value="huey"
-                            checked></input><span className="task mt-4">Take kids to school</span> <span className="time ml-2">8:00-8:30AM</span> <span className="float-right">30min</span> </p>
+                            checked></input><span className="task mt-4">Take kids to school</span><span className="float-right"><i class="fa fa-trash ml-4" aria-hidden="true"></i><i class="fa fa-pencil ml-4" aria-hidden="true"></i><i class="fa fa-calendar ml-4" aria-hidden="true"></i></span> </p>
+
+
                         <p><input style={{ marginRight: "1%" }} type="radio" id="huey" name="drone" value="huey"
-                            checked></input><span className="task">Process email</span>  <span className="time ml-2">8:00-9:30AM</span><span className="float-right">1h</span> </p>
+                            checked></input><span className="task">Process email</span><span className="float-right"><i class="fa fa-trash ml-4" aria-hidden="true"></i><i class="fa fa-pencil ml-4" aria-hidden="true"></i><i class="fa fa-calendar ml-4" aria-hidden="true"></i></span> </p>
+
                         <p> <input style={{ marginRight: "1%" }} type="radio" id="huey" name="drone" value="huey"
-                            checked></input><span className="task">Daily Stand-Up meeting</span><span className="time ml-2">9:00-10:00AM</span> <span className="float-right">30min</span> </p>
+                            checked></input><span className="task">Daily Stand-Up meeting</span><span className="float-right"><i class="fa fa-trash ml-4" aria-hidden="true"></i><i class="fa fa-pencil ml-4" aria-hidden="true"></i><i class="fa fa-calendar ml-4" aria-hidden="true"></i></span> </p>
+
                         <p><input style={{ marginRight: "1%" }} type="radio" id="huey" name="drone" value="huey"
-                            checked></input><span className="task">Create new templates</span><span className="time ml-2">10:00-11:45AM</span><i className="fa fa-trash-o fa-stack-1x fa-inverse"></i> </p>
+                            checked></input><span className="task">Create new templates</span><span className="float-right"><i class="fa fa-trash ml-4" aria-hidden="true"></i><i class="fa fa-pencil ml-4" aria-hidden="true"></i><i class="fa fa-calendar ml-4" aria-hidden="true"></i></span> </p>
+
                         <p><input style={{ marginRight: "1%" }} type="radio" id="huey" name="drone" value="huey"
-                            checked></input><span className="task">Lunch with Anna</span> <span className="time ml-2">12:00-12:30PM</span> <span className="float-right">30min</span> </p>
+                            checked></input><span className="task">Lunch with Anna</span> <span className="float-right"><i class="fa fa-trash ml-4" aria-hidden="true"></i><i class="fa fa-pencil ml-4" aria-hidden="true"></i><i class="fa fa-calendar ml-4" aria-hidden="true"></i></span> </p>
                     </div>
                 </div>
             </div>
